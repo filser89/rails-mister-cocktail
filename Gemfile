7dd0ba0ae228b37cc53ee92d41666e738771317a
Sergey Filippov <sergey.filippov.89@inbox.ru>
2020-07-30 18:49:44 +0800

    completed the crud and started styling

--- Gemfile
+++ Gemfile
@@ -24,9 +24,13 @@ gem 'jbuilder', '~> 2.7'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
-
+gem 'autoprefixer-rails'
+gem 'font-awesome-sass', '~> 5.12.0'
+gem 'simple_form'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,57 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# response = open(url).read
+# d = JSON.parse(response)
+
+# d["drinks"].each do |drink|
+#  i = Ingredient.create!(name: drink["strIngredient1"])
+#   p "created #{i.name}"
+# end
+
+# 10.times do
+#   c = Cocktail.create!(name: Faker::Sports::Football.unique.team)
+#   p "Created #{c}"
+#   d = Dose.create!(description: "100 ml", cocktail: c, ingredient: Ingredient.all.sample)
+#   p "Created #{d} for #{c}"
+# end
+
+
+def createIngredient(drink, c)
+  ingredients = []
+  ingredients.push(drink["strIngredient1"]) unless drink["strIngredient1"].nil?
+  ingredients.push(drink["strIngredient2"]) unless drink["strIngredient2"].nil?
+  ingredients.push(drink["strIngredient3"]) unless drink["strIngredient3"].nil?
+
+  ingredients.each do |ingr|
+    i = Ingredient.find_or_create_by(name: ingr)
+    p i
+    p "Created #{i.name}"
+    doses = ["1 table spoon", "50 ml", "100 ml"]
+    Dose.create!(description: doses.sample, cocktail: c, ingredient: i)
+  end
+end
+
+20.times do |i|
+  # sleep 2
+  cocktail = i + 10
+  url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=110#{cocktail}"
+  response = open(url).read
+  d = JSON.parse(response)
+  p  d
+  next if d["drinks"].nil?
+  drink = d["drinks"][0]
+  c = Cocktail.create!(name: drink["strDrink"], image: drink["strDrinkThumb"])
+  p "Created cocktail #{c.name}"
+  createIngredient(drink, c)
+
+end
+
+puts "Created #{Ingredient.count} ingredients"
+puts "Created #{Cocktail.count} cocktails"
+puts "Created #{Dose.count} doses"
+
